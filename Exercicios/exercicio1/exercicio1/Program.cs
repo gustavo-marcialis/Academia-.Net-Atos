81fc88b0e6ceb26c17ad4ba705066923cb2480bd
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-04 14:46:15 -0300

    Exercicio 1

---
+++ Exercicios/exercicio1/exercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 7;
+            int media = (num1 + num2) / 2;
+            Console.WriteLine(media);
+        }
+    }
+}
