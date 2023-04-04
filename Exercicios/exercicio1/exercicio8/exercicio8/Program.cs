eb6f6f61c017cf5347fa3bc446610db04fcaef16
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-04 16:11:27 -0300

    Exercicio 8

---
+++ Exercicios/exercicio1/exercicio8/exercicio8/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace exercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Temperatura em Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+            double f = (9 * celsius + 160) / 5;
+            Console.WriteLine(f + " Fahrenheit");
+        }
+    }
+}
