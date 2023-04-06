756be2cbe3e8fc2b8b5f0492a8dfb369eaf0ca17
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-06 12:55:26 -0300

    Calculadora de Comissões em C#
    Calculadora de Comissões desenvolvida com Console APP em C#

---
+++ Exercícios/Condicionais if/8 - Calculadora de Comissões em C#/if_8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace if_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo;
+            string nome;
+            double salariobase;
+            double vendas;
+            double salariototal;
+
+            Console.Write("Digite seu codigo: ");
+            codigo = int.Parse(Console.ReadLine());
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Digite seu salario: ");
+            salariobase = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor total das vendas: ");
+            vendas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Codigo: "+codigo);
+            Console.WriteLine("nome: "+nome);
+            Console.WriteLine("salario: "+salariobase);
+            Console.WriteLine("vendas: "+vendas);
+
+            if (vendas > 500)
+            {
+                salariototal = salariobase + vendas + (vendas * 5 / 100);
+                Console.WriteLine("Salario total: "+salariototal);
+            }
+            else if (vendas > 1000)
+            {
+                salariototal = salariobase + vendas + (vendas * 7 / 100);
+                Console.WriteLine("Salario total: " + salariototal);
+            }
+            else if (vendas > 5000)
+            {
+                salariototal = salariobase + vendas + (vendas * 10 / 100);
+                Console.WriteLine("Salario total: " + salariototal);
+            }
+
+        }
+    }
+}
