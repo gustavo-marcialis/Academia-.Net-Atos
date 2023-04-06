f6798c6bc57d086b820448e0815f39f4c0ea664f
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-06 12:29:59 -0300

    Add files via upload

---
+++ Exercícios/exercicio4/exercicio4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Temperatura em Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+            double f = (9 * celsius + 160) / 5;
+            Console.WriteLine(f + " Fahrenheit");
+            
+        }
+    }
+}
