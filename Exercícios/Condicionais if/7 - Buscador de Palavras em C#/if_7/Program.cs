3db13e42e40743abbc5f0c404c097785cedecd6d
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-06 12:53:19 -0300

    Buscador de Palavras em C#
    Buscador de Palavras desenvolvido com Console APP em C#

---
+++ Exercícios/Condicionais if/7 - Buscador de Palavras em C#/if_7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace if_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma frase: ");
+            string frase = Console.ReadLine();
+
+            Console.Write("Digite a palavra a ser buscada: ");
+            string palavra = Console.ReadLine();
+
+            if (frase.Contains(palavra))
+            {
+                Console.WriteLine("A palavra encontra-se na frase.");
+            }
+            else
+            {
+                Console.WriteLine("A palavra não se encontra na frase.");
+            }
+        }
+    }
+}
