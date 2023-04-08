0641ccfd8b6e86c767d4e7c00cdd97d6fa060cd5
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-08 11:20:47 -0300

    Calculadora de Votos

---
+++ Exercícios/11_calcularvotos/11_calcularvotos/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _11_calcularvotos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int votostotais, votosvalidos, votosbrancos, votosnulos;
+            double validosporcento, brancosporcento, nulosporcento;
+
+            
+
+            Console.Write("digite o total de votos: ");
+            votostotais = int.Parse(Console.ReadLine());
+            Console.Write("Votos Brancos: ");
+            votosbrancos = int.Parse(Console.ReadLine());
+            Console.Write("Votos Nulos: ");
+            votosnulos = int.Parse(Console.ReadLine());
+            votosvalidos = votostotais - votosbrancos - votosnulos;
+
+            validosporcento = (votostotais * votosvalidos) / 100;
+            brancosporcento = (votostotais * votosbrancos) / 100;
+            nulosporcento = (votostotais * votosnulos) / 100;
+
+            Console.WriteLine("Votos Válidos: " + validosporcento + "%");
+            Console.WriteLine("Votos Brancos: " + brancosporcento + "%");
+            Console.WriteLine("Votos Nulos: " + nulosporcento + "%");
+
+
+        }
+    }
+}
