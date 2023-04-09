acdedbb2fcdac794b247634f72ef4f452a0d0f12
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-09 12:37:23 -0300

    Calculadora de peso ideal em C#

---
+++ Exercícios/Condicionais Switch Case/13_pesoideal/13_pesoideal/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _13_pesoideal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sexo;
+            double altura, pesoideal;
+
+            Console.Write("Digite seu Sexo (M ou F): ");
+            sexo = Console.ReadLine();
+            Console.Write("Digite sua Altura: ");
+            altura = double.Parse(Console.ReadLine());
+
+            switch (sexo)
+            {
+                case "M":
+                    pesoideal = (72.7 * altura) - 50;
+                    Console.WriteLine("Seu peso ideal é: " + pesoideal.ToString("F2"));
+                break;
+                case "F":
+                    pesoideal = (62.1 * altura) - 44.7;
+                    Console.WriteLine("Seu peso ideal é: " + pesoideal.ToString("F2"));
+                    break;
+                default:
+                    Console.WriteLine("Sexo Inválido!");
+                    break;
+            }
+
+        }
+    }
+}
