d01527763e13dd1d1f637b1ef551965630830b70
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-09 11:58:27 -0300

    Horas Válidas
    Algoritmo que checa se as horas e minutos digitados pelo usuário são válidos.

---
+++ Exercícios/Condicionais if/11_horasvalidas/11_horasvalidas/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _11_horasvalidas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora, minuto;
+
+            Console.Write("Digite a Hora: ");
+            hora = int.Parse(Console.ReadLine());
+            Console.Write("Digite o Minuto: ");
+            minuto = int.Parse(Console.ReadLine());
+
+            if (hora >= 0 && hora < 24) 
+            {
+                if (minuto >= 0 && minuto < 60)
+                {
+                    Console.Write(hora + ":" + minuto);
+                }
+                else
+                {
+                    Console.WriteLine("Hora Inválida");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hora Inválida");
+            }
+
+
+        }
+    }
+}
