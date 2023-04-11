a1ca22317967b298738a21c2a49dd2ee8c2caeba
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-11 12:07:50 -0300

    Fatorial com For

---
+++ Exercícios/Laço de Repetição For/fatorial/fatorial/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace fatorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado, i, fatorial;
+
+            Console.Write("Digite um número: ");
+            fatorial = int.Parse(Console.ReadLine());
+
+            resultado = 1;
+
+            for (i = fatorial; i >= 1; i--)
+            {
+                resultado = resultado * i;
+            }
+            Console.WriteLine("Fatorial de " + fatorial + " é: " + resultado);
+        }
+
+    }
+}
