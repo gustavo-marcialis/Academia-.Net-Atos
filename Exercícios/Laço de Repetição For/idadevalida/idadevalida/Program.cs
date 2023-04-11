757fec83906925970d2e197eba3ac4abd60b1d12
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-11 17:13:28 -0300

    Checagem de idade

---
+++ Exercícios/Laço de Repetição For/idadevalida/idadevalida/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace idadevalida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, i;
+
+            for (i = 0; true; i++)
+            {
+                Console.Write("digite sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+
+                if (idade >0)
+                {
+                    Console.WriteLine("Sua idade é: " + idade + " anos");
+                    break;
+                }
+            }
+        }
+    }
+}
