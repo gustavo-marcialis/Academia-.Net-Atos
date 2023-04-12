d1b57999a5b37c173974c93704426934f8f97f2b
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-12 16:42:46 -0300

    Resolução de Exercício com Do/While
    Resolução do seguinte exercício:
    
    Faça um programa em VS que solicite ao o usuário que informe a idade e valide a entrada de dados, 
    ou seja, repita a leitura até que ela esteja correta (maior do que zero). 
    Ao final, mostre a idade digitada.

---
+++ Exercícios/While/while_2/while_2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace while_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+            do
+            {
+                Console.Write("Digite sua idade: ");
+                idade = int.Parse(Console.ReadLine());
+
+            } while (idade <= 0);
+            Console.WriteLine("idade válida: " + idade);
+        }
+    }
+}
