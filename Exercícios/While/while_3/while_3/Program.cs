40b5d2ac6d76c7cf9563bcc766a50d8ae63d9d96
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-12 17:09:13 -0300

    Resolução de Exercício com While
    Resolução do seguinte exercicício:
    
    Faça um programa em C# que solicite um numero inteiro. Se o numero não for inteiro, solicite-o até que seja. após, informe se ele é par ou impar. 
    Finalmente, pergunte ao usuário se ele quer repetir o programa.

---
+++ Exercícios/While/while_3/while_3/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace while_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string op;
+
+            while (true)
+            {
+                Console.Write("Digite um Número: ");
+                
+                n = int.Parse((Console.ReadLine()));
+
+                if (n <= 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (n % 2 == 0)
+                    {
+                        Console.WriteLine("PAR!");
+                        Console.Write("deseja digitar outro número? s/n: ");
+                        op = Console.ReadLine();
+
+                        if (op == "s")
+                        {
+                            continue;
+                        }
+                        else if (op == "n")
+                        {
+                            break;
+                        }
+                    }
+                    else if ((n % 2) == 1)
+                    {
+                        Console.WriteLine("IMPAR!");
+
+                        Console.Write("deseja digitar outro número? s/n: ");
+                        op = Console.ReadLine();
+
+                        if (op == "s")
+                        {
+                            continue;
+                        }
+                        else if (op == "n")
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
