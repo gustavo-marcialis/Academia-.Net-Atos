7bbce01a2dc2112c2fca7385ad539c80ba3c6c06
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-12 15:15:30 -0300

    Resolução de Exercício com For
    Resolução do seguinte exercício:
    
    Elaborar um programa que apresente o somatório dos valores pares existentes 
    entre 1 e 500.

---
+++ Exercícios/Laço de Repetição For/numerospares/numerospares/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace numerospares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, soma;
+            soma = 0;
+
+            for (i = 1; i <= 500; i++)
+            { 
+                if (i % 2 == 0)
+                {
+                    soma += i;
+                }
+            }
+            Console.WriteLine("A soma de todos os números pares entre 1 e 500 é: " + soma);
+        }
+    }
+}
