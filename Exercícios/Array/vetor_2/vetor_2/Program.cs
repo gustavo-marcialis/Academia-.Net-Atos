7037840b3966ff174edd754397e32dc6f86a2693
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-13 16:00:56 -0300

    Resolução do Exercício 2 de Vetor
    Resolução do seguinte exercício:
    
    Escreva um algoritmo que leia valores para dois vetores de 20 elementos e então realize a soma dos elementos da mesma posição, armazenando o resultado em um outro vetor.

---
+++ Exercícios/Array/vetor_2/vetor_2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace vetor_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor1 = new int[20];
+            int[] vetor2 = new int[20];
+            int[] soma = new int[20];
+            int i = 0, j = 0, k = 0;
+
+
+            for (i = 0; i < vetor1.Length; i++)
+            {
+                Console.Write("Informe um valor para a posição " + i + " do vetor 1: ");
+                vetor1[i] = int.Parse(Console.ReadLine());
+
+                
+            }
+
+            for (j = 0; j < vetor2.Length; j++)
+            {
+                Console.Write("Informe um valor para a posição " + j + " do vetor 2: ");
+                vetor2[j] = int.Parse(Console.ReadLine());
+
+
+            }
+
+            for (k = 0; k < soma.Length; k++)
+            {
+                soma[k] = vetor1[k] + vetor2[k];
+
+                Console.Write("\n" + vetor1[k] + " + " + vetor2[k] + " = " + soma[k]);
+            }
+
+
+
+
+
+
+        }
+    }
+}
