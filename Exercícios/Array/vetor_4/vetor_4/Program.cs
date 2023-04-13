e9d8c050c09850f1a9431929c9c7bc45423d57df
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-13 16:21:40 -0300

    Exercício 4 de vetor
    Resolução do seguinte exercício:
    
    Escreva um algoritmo que leia dois vetores de 10 posições e faça a multiplicação dos elementos de mesmo índice, colocando o resultado em um terceiro vetor. Mostre o vetor resultante.

---
+++ Exercícios/Array/vetor_4/vetor_4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace vetor_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor1 = new int[10];
+            int[] vetor2 = new int[10];
+            int[] resultado = new int[10];
+            int i = 0, j = 0, k = 0;
+
+
+            for (i = 0; i < vetor1.Length; i++)
+            {
+                Console.Write("Informe um valor para a posição " + i + " do vetor 1: ");
+                vetor1[i] = int.Parse(Console.ReadLine());
+
+
+            }
+
+            for (j = 0; j < vetor2.Length; j++)
+            {
+                Console.Write("Informe um valor para a posição " + j + " do vetor 2: ");
+                vetor2[j] = int.Parse(Console.ReadLine());
+
+
+            }
+
+            for (k = 0; k < resultado.Length; k++)
+            {
+                resultado[k] = vetor1[k] * vetor2[k];
+
+                Console.Write("\n" + vetor1[k] + " x " + vetor2[k] + " = " + resultado[k]);
+            }
+        }
+    }
+}
