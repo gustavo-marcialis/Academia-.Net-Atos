2680a584fc68f2d6d8c7ea5d6a883d40fe79a908
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-14 18:06:07 -0300

    Exercício 6 de Vetor
    Resolução do seguinte exercício:
    Fazer um algoritmo que leia vinte números e escreva-os na ordem contrária à ordem de leitura.

---
+++ Exercícios/Vetor/vetor_6/vetor_6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace vetor_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[20];
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.Write($"Digite o {i + 1}º número: ");
+                n[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n ORDEM NORMAL");
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.Write($"{n[i]} ");
+            }
+
+            Console.WriteLine("\n ORDEM INVERSA");
+            for (int i = n.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{n[i]} ");
+            }
+        }
+    }
+}
