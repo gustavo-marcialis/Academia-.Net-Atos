cdf6364c9215e6a7db9a383d08a2227a52e476cd
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-14 17:07:34 -0300

    Add files via upload

---
+++ Exercícios/Matriz/matriz_1/matriz_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace matriz_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[5, 3];
+
+            // i = linha
+            //j = coluna
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.Write("Digite um valor para a coluna " + (i + 1) + ": ");
+                matriz[i, 0] = int.Parse(Console.ReadLine());
+                matriz[i, 1] = matriz[i, 0] + 10;
+                matriz[i, 2] = matriz[i, 0] * 2;
+            }
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
