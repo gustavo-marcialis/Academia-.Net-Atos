f8306940a68a7703df41ac74917b9d32f4336fa1
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-17 15:53:54 -0300

    Exercício 4 de Matriz
    Resolução do seguinte exercício:
    
    Popule uma matriz 5x5 e informe:
    -Quantos números são pares;
    -Quantos números são impares;
    -Quantos números são positivos;
    -Quantos números são negativos;
    -Quantos zeros existem;

---
+++ Exercícios/Matriz/matriz_4/matriz_4/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace matriz_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //i = linhas
+            //j = colunas
+
+            int[,] matriz = new int[5, 5];
+            int pares = 0, impares = 0, zeros = 0, positivos = 0, negativos = 0;
+
+            Console.WriteLine("\n Preencha a matriz 5x5!!");
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write("Posição [" + (i + 1) + "," + (j + 1) + "]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+
+                    if (matriz[i, j] > 0 && matriz[i, j] % 2 == 0)
+                    {
+                        pares += 1;
+
+                        if (matriz[i, j] > 0)
+                        {
+                            positivos += 1;
+                        }
+                    }
+                    else if (matriz[i, j] > 0 && matriz[i, j] % 2 != 0)
+                    {
+                        impares += 1;
+
+                        if (matriz[i, j] > 0)
+                        {
+                            positivos += 1;
+                        }
+                    }
+
+
+                    else if (matriz[i, j] < 0)
+                    {
+                        negativos += 1;
+                    }
+
+                    else if (matriz[i, j] == 0)
+                    {
+                        zeros += 1;
+                    }
+                }
+            }
+
+            Console.WriteLine("Pares: " + pares);
+            Console.WriteLine("Ímpares: " + impares);
+            Console.WriteLine("Positivos: " + positivos);
+            Console.WriteLine("Negativos: " + negativos);
+            Console.WriteLine("Zeros: " + zeros);
+
+        }
+    }
+}
