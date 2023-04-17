df9cccec771fadcf68b8c53d4d984e3052dd67b7
Gustavo Marcialis <76740936+gustavo-marcialis@users.noreply.github.com>
2023-04-17 17:08:02 -0300

    Exercício 9 de Matriz
    Resolução do seguinte exercício:
    Leia uma matriz 3x3.  Em seguida, solicite um número qualquer ao usuário e pesquise na matriz se o número existe.  
    Caso, seja verdade, imprima a mensagem  “O número existe no vetor”, caso contrário, “Número inexistente”.

---
+++ Exercícios/Matriz/Matriz_9/Matriz_9/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Matriz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+            int numero;
+
+            Console.WriteLine("Digite os valores da matriz:\n");
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(0); j++)
+                {
+                    Console.Write($"Posição [{i + 1}, {j + 1}]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.Write("\n Digite um número para pesquisar na matriz: ");
+            numero = int.Parse(Console.ReadLine());
+
+            bool encontrado = false;
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(0); j++)
+                {
+                    if (matriz[i, j] == numero)
+                    {
+                        encontrado = true;
+                        break;
+                    }
+                }
+
+                if (encontrado)
+                {
+                    break;
+                }
+            }
+
+            if (encontrado)
+            {
+                Console.WriteLine("\n O número existe na matriz.");
+            }
+            else
+            {
+                Console.WriteLine("\n Número inexistente na matriz.");
+            }
+        }
+    }
+}
